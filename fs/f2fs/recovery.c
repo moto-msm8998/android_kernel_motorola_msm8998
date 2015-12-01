1ea545cba48a3cf10a412524e0182512e3e78655
Chao Yu <chao2.yu@samsung.com>
2015-12-01 11:43:59 +0800

    f2fs: clean up argument of recover_data
    commit b7973f2378c619d0e17a075f13350bd58a9ebe3d upstream.
    
    In recover_data, value of argument 'type' will be CURSEG_WARM_NODE all
    the time, remove it for cleanup.
    
    Signed-off-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    [bwh: Picked as dependency of commit 6781eabba1bd "f2fs: give -EINVAL for
     norecovery and rw mount"]
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -553,8 +553,7 @@ out:
 	return err;
 }
 
-static int recover_data(struct f2fs_sb_info *sbi, struct list_head *inode_list,
-						struct list_head *dir_list)
+static int recover_data(struct f2fs_sb_info *sbi, struct list_head *head)
 {
 	struct curseg_info *curseg;
 	struct page *page = NULL;
@@ -665,7 +664,7 @@ int f2fs_recover_fsync_data(struct f2fs_sb_info *sbi, bool check_only)
 	need_writecp = true;
 
 	/* step #2: recover data */
-	err = recover_data(sbi, &inode_list, &dir_list);
+	err = recover_data(sbi, &inode_list);
 	if (!err)
 		f2fs_bug_on(sbi, !list_empty(&inode_list));
 skip:
