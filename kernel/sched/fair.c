9d7adb978de3f8e3c12ae0b8ec69158c19d2981a
Yuyang Du <yuyang.du@intel.com>
2015-12-17 07:34:27 +0800

    sched/fair: Fix new task's load avg removed from source CPU in wake_up_new_task()
    [ Upstream commit 0905f04eb21fc1c2e690bed5d0418a061d56c225 ]
    
    If a newly created task is selected to go to a different CPU in fork
    balance when it wakes up the first time, its load averages should
    not be removed from the source CPU since they are never added to
    it before. The same is also applicable to a never used group entity.
    
    Fix it in remove_entity_load_avg(): when entity's last_update_time
    is 0, simply return. This should precisely identify the case in
    question, because in other migrations, the last_update_time is set
    to 0 after remove_entity_load_avg().
    
    Reported-by: Steve Muckle <steve.muckle@linaro.org>
    Signed-off-by: Yuyang Du <yuyang.du@intel.com>
    [peterz: cfs_rq_last_update_time]
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Juri Lelli <Juri.Lelli@arm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Link: http://lkml.kernel.org/r/20151216233427.GJ28098@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4393,15 +4393,23 @@ static inline u64 cfs_rq_last_update_time(struct cfs_rq *cfs_rq)
 #endif
 
 /*
- * Synchronize entity load avg of dequeued entity without locking
- * the previous rq.
+ * Task first catches up with cfs_rq, and then subtract
+ * itself from the cfs_rq (task must be off the queue now).
  */
-void sync_entity_load_avg(struct sched_entity *se)
+void remove_entity_load_avg(struct sched_entity *se)
 {
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
 	u64 last_update_time;
 
+	/*
+	 * Newly created task or never used group entity should not be removed
+	 * from its (source) cfs_rq
+	 */
+	if (se->avg.last_update_time == 0)
+		return;
+
 	last_update_time = cfs_rq_last_update_time(cfs_rq);
+
 	__update_load_avg(last_update_time, cpu_of(rq_of(cfs_rq)), &se->avg, 0, 0, NULL);
 }
 
