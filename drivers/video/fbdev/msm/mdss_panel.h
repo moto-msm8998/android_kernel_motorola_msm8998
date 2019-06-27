7fa318ab588803f745e21d403596a54da69e62d4
SyberHexen <SyberHexen@gmail.com>
2019-06-26 17:05:12 -0700

    Revert "video: mdss: LiveDisplay driver"
    * We don't need this for CABC
    
    This reverts commit 6032360e82ef904e74fa59b71506b2dbe9f6deec.
    
    Change-Id: Iaa1058e582ba8449a69f51432750348ecb7b8c95

--- drivers/video/fbdev/msm/mdss_panel.h
+++ drivers/video/fbdev/msm/mdss_panel.h
@@ -374,7 +374,6 @@ enum mdss_intf_events {
 	MDSS_EVENT_DSI_DYNAMIC_BITCLK,
 	MDSS_EVENT_MAX,
 	MDSS_EVENT_ENABLE_TE,
-	MDSS_EVENT_UPDATE_LIVEDISPLAY,
 };
 
 /**
@@ -831,8 +830,6 @@ struct mdss_dsi_dual_pu_roi {
 	bool enabled;
 };
 
-struct mdss_livedisplay_ctx;
-
 struct mdss_panel_hdr_properties {
 	bool hdr_enabled;
 
@@ -996,8 +993,6 @@ struct mdss_panel_info {
 	 * configuring the event timer wakeup logic.
 	 */
 	u32 adjust_timer_delay_ms;
-
-	struct mdss_livedisplay_ctx *livedisplay;
 
 	/* debugfs structure for the panel */
 	struct mdss_panel_debugfs_info *debugfs_info;
